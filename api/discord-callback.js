ca08d214cf1b13f3216ad2cdb292f1789341299d
elplaza26 <gabrielroger.app@gmail.com>
2026-08-08 20:03:07 +0200

    Update discord-callback.js

--- api/discord-callback.js
+++ api/discord-callback.js
@@ -1,3 +1,118 @@
+Skip to content
+elplaza26
+panel-asuntos-internos
+Repository navigation
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security and quality
+1
+ (1)
+Insights
+Settings
+Files
+Go to file
+t
+T
+api
+discord-callback.js
+index.html
+panel-asuntos-internos/api
+/
+discord-callback.js
+in
+main
+
+Edit
+
+Preview
+Indent mode
+
+Spaces
+Indent size
+
+2
+Line wrap mode
+
+No wrap
+Editing discord-callback.js file contents
+  1
+  2
+  3
+  4
+  5
+  6
+  7
+  8
+  9
+ 10
+ 11
+ 12
+ 13
+ 14
+ 15
+ 16
+ 17
+ 18
+ 19
+ 20
+ 21
+ 22
+ 23
+ 24
+ 25
+ 26
+ 27
+ 28
+ 29
+ 30
+ 31
+ 32
+ 33
+ 34
+ 35
+ 36
+ 37
+ 38
+ 39
+ 40
+ 41
+ 42
+ 43
+ 44
+ 45
+ 46
+ 47
+ 48
+ 49
+ 50
+ 51
+ 52
+ 53
+ 54
+ 55
+ 56
+ 57
+ 58
+ 59
+ 60
+ 61
+ 62
+ 63
+ 64
+ 65
+ 66
+ 67
+ 68
+ 69
+ 70
+ 71
+ 72
+ 73
+ 74
 export default async function handler(req, res) {
   const { code } = req.query;
 
@@ -71,3 +186,6 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: 'Error interno en el servidor', detalle: String(e) });
   }
 }
+
+Use Control + Shift + m to toggle the tab key moving focus. Alternatively, use esc then tab to move to the next interactive element on the page.
+Copied!
